67cd80b8a2c34a58180187e2bad68e4b50047b6d
KenRoyy <davidcocoj@gmail.com>
2023-05-04 23:21:58 -0500

    fix-contenido-segundario-noticias

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
 // const animado = document.querySelector('.news-animado');
-let animado = document.getElementById('text');
+let animado = document.querySelector('#text');
+let span_anim = document.querySelector('#span-anim');
+let title_anim = document.querySelector('#span-anim h3');
 
-window.addEventListener("scroll", ()=>{
-    let pantalla = window.innerHeight;
-    let ubicacion = animado.getBoundingClientRect().top;
-  
-    console.log(pantalla ,ubicacion);
-  
-    if(ubicacion <= pantalla){
-        animado.classList.add('anim-text');
-        console.log('cambio de color?');
-    }
-})
+console.log(title_anim);
+
+function animText(animado,anim) {   
+    window.addEventListener("scroll", ()=>{
+        let pantalla = window.innerHeight;
+        let ubicacion = animado.getBoundingClientRect().top;
+        
+        // console.log(pantalla ,ubicacion);
+        
+        if(ubicacion <= pantalla){
+            // animado.classList.add('anim-text');
+            animado.classList.add(anim)
+            console.log('cambio de color?');
+        }
+    })
+}
+
+animText(animado,'anim-text')
+animText(span_anim,'anim_span')
+animText(title_anim, 'anim_span')
+
+
