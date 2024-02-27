312aff5472937274f4bad546d2c3420f773bab6b
KenRoyy <davidcocoj@gmail.com>
2024-02-27 14:12:24 -0500

    parche con minimos cambios: se añadio un js es para solo mandar mensajes de alerta/ cambio del correo

---
+++ js/event.js
@@ -0,0 +1,4 @@
+const message = (e) => {
+    e.preventDefault();
+    alert('la página está en mantenimiento. Si quieres contactarnos o obtener más información de buclex presiona el botón de whatsapp');
+}
