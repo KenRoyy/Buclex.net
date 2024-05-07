d2d6f331ad1be2259a88ca10c79bd0854eac4080
KenRoyy <davidcocoj@gmail.com>
2024-05-06 19:11:56 -0500

    rediseño buclex

--- js/descriptionAnim.js
+++
@@ -1,7 +0,0 @@
-const title = document.querySelector("#title");
-
-const handleClick = () => {
-  console.log("click");
-};
-
-title.addEventListener('click', handleClick);
--- js/slider/swiper.js
+++ js/slider/swiper.js
@@ -1,9 +1,21 @@
-import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
 const swiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 2500,
   },
-  direction: "horizontal",
+  pagintaion: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  }
 });
